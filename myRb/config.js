496fcfd07255682fb51a5e34f15bb01627ede609
Spantioc Radu <cosmin.spantioc@cpp.canon>
2022-10-21 11:55:44 +0300

    re-start

---
+++ myRb/config.js
@@ -0,0 +1,75 @@
+module.exports = {
+  platform: 'github',
+  onboardingConfig: {
+    extends: ['config:base'],
+  },
+  repositories: ['raduspa/renovateTest'],
+  requireConfig: "optional",
+  onboarding: false,  
+  logLevel: 'debug',
+  recreateClosed: true,
+  dependencyDashboard: true,
+  
+  extends: ["config:base",
+    "group:allNonMajor",
+    ":preserveSemverRanges"
+  ],
+  lockFileMaintenance: {
+    "enabled": true
+  },
+  baseBranches: [
+    "main"
+  ],
+  ignorePaths: [
+    "**/node_modules/**",
+    "**/test/**",
+    "**/tests/**"
+  ],
+  packageRules: [
+    {
+      "description": "explicitly enable npm",
+      "matchManagers": [
+        "npm"
+      ],
+      "matchPackagePatterns": [
+        "*"
+      ],
+      "matchCurrentVersion": "!/.*-.*/",
+      "enabled": true
+    },
+    {
+      "description": "Pin typescript for angular 12",
+      "matchPackageNames": ["typescript"],
+      "allowedVersions": "<=4.3.5"
+    },
+    {
+      "description": "Pin angular 12",
+      "matchPackagePatterns": ["angular"],
+      "allowedVersions": "<13"
+    },
+    {
+      "description": "first disable all the default maven updates",
+      "matchManagers": [
+        "gradle"
+      ],
+      "enabled": false
+    },
+    {
+      "description": "explicitly enable maven",
+      "matchManagers": [
+        "gradle"
+      ],
+      "matchPackagePatterns": [
+        "^org.kuali."
+      ],
+      "enabled": true
+    },
+    {
+      "description": "Disable for global.json",
+      "matchFiles": [
+        "global.json"
+      ],
+      "enabled": false
+    }
+  ]
+};
